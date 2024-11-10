efeb72e5ae196e287fc11d8dc2b42b921871658b
Kode Navigator <chumex412@gmail.com>
2024-11-10 09:15:50 +0100

    reverts to empty tasks state

--- src/modules/tasks/RenderTaskContent.tsx
+++ src/modules/tasks/RenderTaskContent.tsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import Tasks from ".";
-import Loader from "../../components/Loader";
 import { TodoTabType } from "../../entities/modules";
 import { Task } from "../../entities/ui";
 
 const todoTabs: TodoTabType[] = ["All", "Completed", "Pending"];
 
 const RenderContent = () => {
-  // Replace the lines below with the variables destructured from API query
-  const tasks: Task[] = [];
-  const isLoading = false;
-  const isSuccess = true;
-  //
+  const tasks = [] as Task[];
   const [todoTab, setTodoTab] = useState<TodoTabType>("All");
   const [filteredTasks, setFilteredTasks] = useState<Task[] | null>();
 
@@ -33,32 +28,25 @@ const RenderContent = () => {
     }
   };
 
-  return isLoading ? (
-    <div className="flex h-[50vh] items-center justify-center text-[30px]">
-      <Loader />
-    </div>
-  ) : isSuccess ? (
-    <>
-      <section className="tab-container flex justify-center gap-8 text-center">
-        {todoTabs.map((tab) => {
-          return (
-            <button
-              key={tab}
-              onClick={() => sortTasks(tab)}
-              className={`cursor-pointer rounded-none border-0 border-b-2 bg-transparent ${todoTab === tab ? "border-accent text-accent focus:border-accent" : "border-secondary text-secondary focus:border-primary"} focus-within::border-0 focus:border-0 focus:border-b focus:outline-none`}
-            >
-              {tab}
-            </button>
-          );
-        })}
-      </section>
-      <section className="mt-8">
-        <Tasks tasks={filteredTasks || tasks} tab={todoTab} />
-      </section>
-    </>
-  ) : (
-    <p className="text-center text-2xl leading-[150%]">Failed to fetch</p>
-  );
+  return;
+  <>
+    <section className="tab-container flex justify-center gap-8 text-center">
+      {todoTabs.map((tab) => {
+        return (
+          <button
+            key={tab}
+            onClick={() => sortTasks(tab)}
+            className={`cursor-pointer rounded-none border-0 border-b-2 bg-transparent ${todoTab === tab ? "border-accent text-accent focus:border-accent" : "border-secondary text-secondary focus:border-primary"} focus-within::border-0 focus:border-0 focus:border-b focus:outline-none`}
+          >
+            {tab}
+          </button>
+        );
+      })}
+    </section>
+    <section className="mt-8">
+      <Tasks tasks={filteredTasks || tasks} tab={todoTab} />
+    </section>
+  </>;
 };
 
 export default RenderContent;
